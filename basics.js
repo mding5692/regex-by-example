99a86b466bd24390871c0923a59d31b32022c7a8
Michael <m.ding5692@gmail.com>
2015-08-28 00:57:21 -0400

    Update basics.js

--- basics.js
+++ basics.js
@@ -1,1 +1,8 @@
+// regex is usually read by regex engines in each programming language
 
+\d = digit from 0 to 9
+\w = word that can be letter, digit or underscore
+\s = whitespace, newline, tab or carriage return
+\D = not a digit, aka anything not \d
+\W = anything that is not \w
+\S = anything not \s
